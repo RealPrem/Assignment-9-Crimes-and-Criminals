3c658ce40063ec26b862284b0bf37010999496c5
prem <premplayz@gmail.com>
2021-02-01 21:56:00 +0700

    HW Update

--- Assignment 9 Crimes and Criminals/Program.cs
+++ Assignment 9 Crimes and Criminals/Program.cs
@@ -12,9 +12,31 @@ namespace Assignment_9_Crimes_and_Criminals
             Criminals[1] = new Criminal("Jesus", 51);
             Criminals[2] = new Criminal("John", 26);
 
-            Criminals[0].AddCrime(new Crime(DateTime.Parse("31/01/2020"), "Ascot", "Stealng"));
+            Criminals[0].AddCrime(new Crime(DateTime.Parse("31/01/2020"), "Ascot", "Stealing"));
             Criminals[0].AddCrime(new Crime(DateTime.Parse("30/01/2020"), "Airport", "Stole dog"));
 
+            Criminals[1].AddCrime(new Crime(DateTime.Parse("01/02/2021"), "Ascot", "Talking to Inn"));
+
+            Criminals[2].AddCrime(new Crime(DateTime.Parse("01/02/2021"), "Ascot", "Being Friends with Inn"));
+
+
+            for (int i = 0; i < Criminals.Length; i += 1)
+            {
+                int TotalCrime = Criminals[i].CountCrime();
+                if (TotalCrime > 1)
+                {
+                    Console.WriteLine("CRIMINAL: " + (i + 1));
+                    Console.WriteLine("EXECUTE");
+                }
+                else
+                {
+                    Console.WriteLine("CRIMINAL: " + (i + 1));
+                    for (int y = 0; y < TotalCrime; y += 1)
+                    {
+                        Console.WriteLine("ACT: " + Criminals[i].GetCrime(y).GetAct() + " "  + "LOCATION: " +  Criminals[i].GetCrime(y).GetLocation());
+                    }
+                }
+            }
             // HW
             // Add Crimes to all 3 Criminals
             // Write a for loop below which loop through all 3 criminals
